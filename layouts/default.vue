8785c243596c5552565c904c2a5e2e52254cb86c
ioanb7 <ioanb7code@gmail.com>
2020-06-11 21:25:31 +0100

    refactorings, updated cv

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,66 @@
 <template>
   <div>
-    <nuxt />
+    <div class="container">
+      <div>
+        <h1 class="title">
+          Ioan
+        </h1>
+        <h2 class="subtitle">
+          Contractor
+        </h2>
+        <nuxt />
+        <div class="links">
+          <CustomButton text="Ioan's CV" afterClickText="Ioan's CV - Downloaded" @click="cvClick" icon="attach_file"
+            buttonId="1" />
+          <CustomButton text="Ioan's LinkedIn" @click="linkedinClick" icon="linkedin" buttonId="2" />
+          <CustomButton text="Ioan's blog" @click="blogClick" icon="crop_free" buttonId="3" />
+        </div>
+      </div>
+    </div>
+
     <footer>
       <cookie-law theme="dark-lime"></cookie-law>
     </footer>
   </div>
 </template>
+
+<script>
+  import CustomButton from '~/components/CustomButton'
+  import axios from 'axios'
+  export default {
+    components: {
+      CustomButton
+    },
+    methods: {
+      async cvClick() {
+        let url = "/cv.pdf"
+        let label = "IoanBiticu-Contractor.pdf"
+
+        var response = await axios.get(url, {
+          responseType: 'blob'
+        })
+        const blob = new Blob([response.data], {
+          type: 'application/pdf'
+        })
+        const link = document.createElement('a')
+        link.href = URL.createObjectURL(blob)
+        link.download = label
+        link.click()
+        URL.revokeObjectURL(link.href)
+      },
+      async linkedinClick() {
+        const linkedInUrl = "https://www.linkedin.com/in/ioanbiticu/"
+        window.open(linkedInUrl, '_blank');
+      },
+      async blogClick() {
+        const blogUrl = "https://ioan.blog/"
+        window.open(blogUrl, '_blank');
+      }
+    }
+  }
+
+</script>
+
 
 <style>
   html {
@@ -20,40 +75,35 @@
     box-sizing: border-box;
   }
 
-  *,
-  *:before,
-  *:after {
-    box-sizing: border-box;
-    margin: 0;
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
   }
 
-  .button--green {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #3b8070;
-    color: #3b8070;
-    text-decoration: none;
-    padding: 10px 30px;
-  }
-
-  .button--green:hover {
-    color: #fff;
-    background-color: #3b8070;
-  }
-
-  .button--grey {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #35495e;
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+      'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
     color: #35495e;
-    text-decoration: none;
-    padding: 10px 30px;
-    margin-left: 15px;
+    letter-spacing: 1px;
   }
 
-  .button--grey:hover {
-    color: #fff;
-    background-color: #35495e;
+  .subtitle {
+    font-weight: 300;
+    font-size: 24px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
   }
 
 </style>
