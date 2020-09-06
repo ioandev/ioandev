ece0c8810200c5c3585c97ab97275b09da3c29b8
Ioan Biticu <ioanb7code@gmail.com>
2020-09-06 13:41:26 +0100

    Added robots noindex

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'robots', content: 'noindex'}
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/favicon.png' },
