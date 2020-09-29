3ac30121b9e5e4cc347192d01405d6ef46881e1e
Ioan Biticu <ioanb7code@gmail.com>
2020-09-29 21:37:23 +0100

    fix no robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { name: 'robots', content: 'noindex'}
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/favicon.png' },
